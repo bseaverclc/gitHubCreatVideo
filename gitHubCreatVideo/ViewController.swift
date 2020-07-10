e0162b900385aa4f9e8473ee18aa46076cf3d4d5
Brian Seaver <bseaver@d155.org>
2020-07-10 10:39:49 -0500

    Initial commit

---
+++ gitHubCreatVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubCreatVideo
+//
+//  Created by Brian Seaver on 7/10/20.
+//  Copyright © 2020 clc.seaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+// Testing Git Hub
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
